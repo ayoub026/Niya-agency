4d7b0a7ac15734aa54254cacbe9ae3364d54d0bd
ayoub026 <ayoubzraa@icloud.com>
2025-01-09 01:12:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.btn-primary').forEach(button => {
+    button.addEventListener('click', function () {
+        alert('Bedankt voor je interesse! We nemen snel contact met je op.');
+    });
+})
