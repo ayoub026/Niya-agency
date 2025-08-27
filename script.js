58c11b7312fad75ae12a6157067da445ca7360d2
ayoub026 <ayoubzraa@icloud.com>
2025-08-27 21:11:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.querySelectorAll('.btn-primary').forEach(button => {
         alert('Bedankt voor je interesse! We nemen snel contact met je op.');
     });
 })
+document.addEventListener("DOMContentLoaded", function () {
+    // Initialiseer Leaflet-kaart
+    var map = L.map('map').setView([51.950637202844966, 5.897669917177035], 16);
+  
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+  
+    var marker = L.marker([51.950637202844966, 5.897669917177035]).addTo(map);
+    marker.bindPopup("<b>Niya Agency</b><br>Groningsingel 1191").openPopup();
