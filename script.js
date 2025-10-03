6a56b0b1a349996b368bc788b937cdaebf7b7341
botanAb <botanabak@gmail.com>
2025-10-04 01:44:40 +0200

    bugfixes

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-document.querySelectorAll('.btn-primary').forEach(button => {
-    button.addEventListener('click', function () {
-        alert('Bedankt voor je interesse! We nemen snel contact met je op.');
-    });
-})
-document.addEventListener("DOMContentLoaded", function () {
-    // Initialiseer Leaflet-kaart
-    var map = L.map('map').setView([51.950637202844966, 5.897669917177035], 16);
-  
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+document.querySelectorAll(".btn-primary").forEach((button) => {
+  button.addEventListener("click", function () {
+    alert("Bedankt voor je interesse! We nemen snel contact met je op.");
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const mapDiv = document.getElementById("map");
+  if (mapDiv) {
+    var map = L.map("map").setView([51.950637202844966, 5.897669917177035], 16);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 19,
-      attribution: '&copy; OpenStreetMap contributors'
+      attribution: "&copy; OpenStreetMap contributors",
     }).addTo(map);
-  
+
     var marker = L.marker([51.950637202844966, 5.897669917177035]).addTo(map);
     marker.bindPopup("<b>Niya Agency</b><br>Groningsingel 1191").openPopup();
+  }
+});
